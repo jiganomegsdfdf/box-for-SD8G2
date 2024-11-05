d290bcb3b61edf29f5295f43c52ea2eb80bbb354
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-11-05 23:00:09 +0300

    Fix box_build.sh

--- box_build.sh
+++ box_build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ptitSeb/box64 --depth 1
 cd box64
 mkdir build
 cd build
-cmake .. -DSD8G2=1 
+cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/aarch64-linux-gnu-g++ -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc -DSD8G2=1 
 make -j$(nproc)
 sudo make install DESTDIR=../../debs/
 
@@ -12,8 +12,9 @@ cd ../../box86
 patch -p1 < ../patches/box86.patch
 mkdir build
 cd build
-cmake .. -DSD8G2=1
+cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabi-g++ -DCMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabi-gcc -DSD8G2=1
 make -j$(nproc)
 sudo make install DESTDIR=../../debs/
+cd ../../
 
 dpkg-deb --build --root-owner-group debs
