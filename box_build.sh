45f7c26339e8845a75e02a8dccb216d11710732b
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-11-05 23:42:55 +0300

    Update box_build.sh

--- box_build.sh
+++ box_build.sh
@@ -6,7 +6,7 @@ mkdir build
 cd build
 cmake .. -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc-14 -DSD8G2=1 
 make -j$(nproc)
-sudo make install DESTDIR=../../debs/
+make install DESTDIR=../../debs/
 
 cd ../../box86
 patch -p1 < ../patches/box86.patch
@@ -14,7 +14,7 @@ mkdir build
 cd build
 cmake .. -DCMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabi-gcc-14 -DSD8G2=1
 make -j$(nproc)
-sudo make install DESTDIR=../../debs/
+make install DESTDIR=../../debs/
 cd ../../
 
 dpkg-deb --build --root-owner-group debs
