519dcff41a4f58127de47e01579a2b3286d76fb1
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-11-06 09:47:29 +0300

    Update box_build.sh

--- box_build.sh
+++ box_build.sh
@@ -6,7 +6,7 @@ cp $1/*hard* /usr/arm-linux-gnueabi/include/gnu/
 cd box64
 mkdir build
 cd build
-cmake .. -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc-14 -DSD8G2=1 
+cmake .. -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc-14 -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_CROSSCOMPILING=TRUE -DSD8G2=1 
 make -j$(nproc)
 make install DESTDIR=../../debs/
 
@@ -14,7 +14,7 @@ cd ../../box86
 patch -p1 < ../patches/box86.patch
 mkdir build
 cd build
-cmake .. -DCMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabi-gcc-14 -DSD8G2=1
+cmake .. -DCMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabi-gcc-14 -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_CROSSCOMPILING=TRUE -DSD8G2=1
 make -j$(nproc)
 make install DESTDIR=../../debs/
 cd ../../
